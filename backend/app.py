e00a3befa1e64adc536b1dc8a2326785db6c25a2
G Nehru <nehrugannamaneni@gmail.com>
2026-06-18 21:18:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,53 @@
-
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import tempfile
 import os
 
 from predictor import predict_banana
 
-app = FastAPI()
+app = FastAPI(title="RipenIQ API")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
-    return {"message": "Banalyzer API running"}
+    return {
+        "message": "RipenIQ API Running"
+    }
+
+@app.get("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp:
+
+    suffix = os.path.splitext(file.filename)[1]
+
+    with tempfile.NamedTemporaryFile(
+        delete=False,
+        suffix=suffix
+    ) as temp:
+
         temp.write(await file.read())
-        image_path = temp.name
+        temp_path = temp.name
 
-    result = predict_banana(image_path)
+    try:
+        result = predict_banana(temp_path)
+        return result
 
-    os.remove(image_path)
-
-    return result
+    finally:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
